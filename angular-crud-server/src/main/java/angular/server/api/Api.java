84fa868d0c4642cbe6c89a7a603959116acbd65c
Jorge Eduardo Rossetto <jorge.rosseto@serpro.gov.br>
2018-04-24 11:06:42 -0300

    Permitindo CORS.

--- angular-crud-server/src/main/java/angular/server/api/Api.java
+++ angular-crud-server/src/main/java/angular/server/api/Api.java
@@ -1,9 +1,37 @@
 package angular.server.api;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+import org.jboss.resteasy.plugins.interceptors.CorsFilter;
+
 @ApplicationPath("api")
 public class Api extends Application {
+
+	private Set<Object> singletons = new HashSet<Object>();
+	
+	private HashSet<Class<?>> classes = new HashSet<Class<?>>();
+
+	public Api() {
+		CorsFilter corsFilter = new CorsFilter();
+		corsFilter.getAllowedOrigins().add("*");
+		corsFilter.setAllowedMethods("OPTIONS, GET, POST, DELETE, PUT, PATCH");
+		singletons.add(corsFilter);
+
+		classes.add(UsuarioResource.class);
+	}
+
+	@Override
+	public Set<Object> getSingletons() {
+		return singletons;
+	}
+
+	@Override
+	public HashSet<Class<?>> getClasses() {
+		return classes;
+	}
 
 }
