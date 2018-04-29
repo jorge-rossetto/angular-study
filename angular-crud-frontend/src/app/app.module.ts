eb0692ccd480a8ef703f0fc046f535ee172f3bae
Jorge Eduardo Rossetto <jorgerst@gmail.com>
2018-04-29 11:07:49 -0300

    Habilitando HttpClientModule.

--- angular-crud-frontend/src/app/app.module.ts
+++ angular-crud-frontend/src/app/app.module.ts
@@ -5,18 +5,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { UsuarioCreateComponent } from './components/usuario-create/usuario-create.component';
+import { UsuarioReadAllComponent } from './components/usuario-read-all/usuario-read-all.component';
+
+import { UsuarioService } from './services/usuario.service';
+import { HttpClientModule } from '@angular/common/http';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsuarioCreateComponent,
+    UsuarioReadAllComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     MaterializeModule
   ],
-  providers: [],
+  providers: [UsuarioService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
