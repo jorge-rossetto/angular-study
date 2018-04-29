7cebd7fb083d7c8df655cc8c2ce1bf994ac8fb13
Jorge Eduardo Rossetto <jorgerst@gmail.com>
2018-04-29 18:18:26 -0300

    usuario-update (versão crua).

--- angular-crud-frontend/src/app/components/usuario-create-or-update/usuario-create-or-update.component.ts
+++ angular-crud-frontend/src/app/components/usuario-create-or-update/usuario-create-or-update.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Usuario } from '../../model/usuario.model';
 import { UsuarioService } from '../../services/usuario.service';
+import { isNumeric } from "rxjs/util/isNumeric"
 
 @Component({
   selector: 'app-usuario-create-or-update',
@@ -9,17 +10,73 @@ import { UsuarioService } from '../../services/usuario.service';
 })
 export class UsuarioCreateOrUpdateComponent implements OnInit {
 
+  id: any;
+  updateMode: boolean;
+  submitButtonTitle: string;
   usuario: Usuario = new Usuario();
 
-  constructor(private router: Router, private usuarioService: UsuarioService) {
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, private usuarioService: UsuarioService) {
   }
 
   ngOnInit() {
+    this.storeIdParameter();
+    this.setCreateOrUpdateMode();
+    this.setSubmitButtonTitle();
+    this.initializeModel();
+  }
+
+  storeIdParameter() {
+    this.id = this.activatedRoute.snapshot.queryParams['id'];
+    console.log('query param "id":', this.id);
+  }
+
+  setCreateOrUpdateMode() {
+    this.updateMode = isNumeric(this.id);
+  }
+
+  setSubmitButtonTitle() {
+    if (this.updateMode) {
+      this.submitButtonTitle = 'Alterar';
+    } else {
+      this.submitButtonTitle = 'Incluir';
+    }
+  }
+
+  initializeModel() {
+    if (this.updateMode) {
+      this.initializeExistingModelById();
+    } else {
+      this.initializeNewModel();
+    }
+  }
+
+  initializeExistingModelById() {
+    this.usuarioService.readById(this.id).
+    subscribe(
+      retorno => {console.log('successo: ', retorno); this.usuario = retorno},
+      erro => console.log('erro: ', erro) // TODO não seria melhor tratar erros só nos services?
+    );
+
+    // TODO quando usuario passou um id que não tem nenhum usuario, mandar de volta pra pagina de consulta, com mensagem de erro
+
+  }
+
+  initializeNewModel() {
+    this.usuario = new Usuario();
   }
 
   onSubmit() {
-    this.createUsuario(); 
+    if (this.updateMode) {
+      this.updateUsuario();
+    } else {
+      this.createUsuario();
+    }
+
     this.backToReadAllPage();
+  }
+
+  updateUsuario() {
+    this.usuarioService.update(this.usuario);
   }
 
   createUsuario() {
--- angular-crud-frontend/src/app/services/usuario.service.ts
+++ angular-crud-frontend/src/app/services/usuario.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
@@ -8,34 +8,38 @@ import { catchError, retry, tap, map } from 'rxjs/operators';
 @Injectable()
 export class UsuarioService {
 
-    readonly resourceUrl: string = 'http://localhost:8080/angular-crud-server/api/usuario';
-    
+    readonly resourceUrl: string = 'http://localhost:8080/angular-crud-server/api/usuario/';
+
     // pelo jeito não preciso mandar os headers, mas se precisar tá aqui:
     // contentTypeJsonHeaders: HttpHeaders = new HttpHeaders().set("content-type", "application/json");
+
+    // se precisar passar query params:
+    // const options = { params: new HttpParams().set('id', id.toString()), reportProgress: true };
+    // console.log(options.params.toString());
 
     constructor(private http: HttpClient) {
     }
 
-    readAll() {
+    readAll(): Observable<Usuario[]> {
         return this.http.get<Usuario[]>(this.resourceUrl);
     }
 
-    create(usuario: Usuario) {
+    readById(id: number): Observable<Usuario> {
+
+        return this.http.get<Usuario>(this.resourceUrl + id);
+    }
+
+    create(usuario: Usuario): void {
         // TODO tentar usar pipe(), parece que eh o jeito mais moderno
         this.http.post(this.resourceUrl, usuario).subscribe(
             response => console.dir(response), error => console.dir(error)
         );
+    }
 
-    
-        // const headers:HttpHeaders = new HttpHeaders().set("content-type", "application/json");
-        // return this.http.post<Usuario>(this.resourceUrl, usuario, '').pipe(tap( // Log the result or error
-        //     data => console.log(data),
-        //     error => console.log(error)
-        //   ));
-
-        //   .pipe(
-        //     catchError(this.handleError('addHero', hero))
-        //   );
+    update(usuario: Usuario): void {
+        this.http.put(this.resourceUrl, usuario).subscribe(
+            response => console.dir(response), error => console.dir(error)
+        );
     }
 
 }
