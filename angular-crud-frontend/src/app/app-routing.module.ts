3cd13d3685e9318d7de946464c48e674a11166ac
Jorge Eduardo Rossetto <jorge.rosseto@serpro.gov.br>
2018-05-02 18:25:10 -0300

    Separando tela com consulta básica de Usuario.

--- angular-crud-frontend/src/app/app-routing.module.ts
+++ angular-crud-frontend/src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UsuarioCreateOrUpdateComponent } from './components/usuario-create-or-update/usuario-create-or-update.component';
 import { UsuarioReadAllComponent } from './components/usuario-read-all/usuario-read-all.component';
+import { UsuarioReadAllBasicoComponent } from './components/usuario-read-all-basico/usuario-read-all-basico.component';
 
 const routes: Routes = [
   {
     path: 'usuario-read-all',
     component: UsuarioReadAllComponent
+  },
+  {
+    path: 'usuario-read-all-basico',
+    component: UsuarioReadAllBasicoComponent
   },
   {
     path: 'usuario-create-or-update',
--- angular-crud-frontend/src/app/app.module.ts
+++ angular-crud-frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { UsuarioService } from './services/usuario.service';
 import { HttpClientModule } from '@angular/common/http';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { SingleFieldFilterPipe } from './infra/single-field-filter.pipe';
+import { UsuarioReadAllBasicoComponent } from './components/usuario-read-all-basico/usuario-read-all-basico.component';
 
 
 @NgModule({
@@ -22,6 +23,7 @@ import { SingleFieldFilterPipe } from './infra/single-field-filter.pipe';
     UsuarioCreateOrUpdateComponent,
     UsuarioReadAllComponent,
     SingleFieldFilterPipe,
+    UsuarioReadAllBasicoComponent,
   ],
   imports: [
     MaterializeModule,
